e045f2de01f4dafcb41c813bd053c37a8bc52e78
robekenn <robekenn@oregonstate.edu>
2026-02-10 13:54:54 -0800

    Test1

--- BeavGredients/src/main.jsx
+++ BeavGredients/src/main.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import express from "express";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => res.json({ ok: true }));
+
+export default app;
