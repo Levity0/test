8488040b73665a8dd14b64687340fa7b59b02eaf
Alexander Sahlstrom <Alexander.Sahlstrom@gmail.com>
2026-02-17 14:14:51 -0800

    Implmented simple data organization and added some comments

--- Web/server/index.js
+++ Web/server/index.js
@@ -19,9 +19,18 @@ app.get("/api/main", async (req, res) => {
     // if (!mealName){
     //   return res.status(400).json({error: "please provide meal name"}); 
     // }
+    // fetch the data from meal db
     const response = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=a'); //www.themealdb.com/api/json/v1/1/search.php?f=a
-    res.json(response.data);
-  }catch(error) {
+    // organize data fetched from TheMealDB
+    const organizedMeals = response.data.meals.map (meal =>({ 
+      id: meal.idMeal,
+      name: meal.strMeal,
+      category: meal.strCategory,
+      area: meal.strArea
+
+    }));
+    res.json(organizedMeals);
+  }catch(error) { // catch errors from meal db
     console.error("Error fetching meal data from TheMealDB", error);
     res.status(500).json({error: "internal server error"});
   }
