deb8cda54376d3cc88ea67dfaabfd678f9e61f67
Alexander Sahlstrom <Alexander.Sahlstrom@gmail.com>
2026-02-21 21:52:51 -0800

    Implemented backend search functionality

--- Web/server/index.js
+++ Web/server/index.js
@@ -12,7 +12,7 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
 
-// respond to request asking for meal
+// display meals 
 app.get("/api/main", async (req, res) => {
   try {
     // const { mealName } = req.query;
@@ -26,7 +26,36 @@ app.get("/api/main", async (req, res) => {
       id: meal.idMeal,
       name: meal.strMeal,
       category: meal.strCategory,
-      area: meal.strArea
+      area: meal.strArea,
+      recipe: meal.strInstructions
+
+    }));
+    res.json(organizedMeals);
+  }catch(error) { // catch errors from meal db
+    console.error("Error fetching meal data from TheMealDB", error);
+    res.status(500).json({error: "internal server error"});
+  }
+});
+
+// display meals based on search
+app.get("/api/search", async (req, res) => {
+  try {
+    const { mealName }  = req.query;
+    if (!mealName){
+      return res.status(400).json({error: "please provide meal name"}); 
+    }
+    // fetch the data from meal db
+    const response = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?', {
+      params: { s: mealName }
+    }); 
+    // organize data fetched from TheMealDB
+    const rawMealsData = response.data.meals || [];
+    const organizedMeals = rawMealsData.map (meal =>({ 
+      id: meal.idMeal,
+      name: meal.strMeal,
+      category: meal.strCategory,
+      area: meal.strArea,
+      recipe: meal.strInstructions
 
     }));
     res.json(organizedMeals);
