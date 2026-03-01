eff812facc1f90bcd36125d8cc8b3b0d0ec20c27
Alexander Sahlstrom <Alexander.Sahlstrom@gmail.com>
2026-02-28 16:41:09 -0800

    removed test since this change doesnt  relate to this branch

--- Web/__tests__/page.test.jsx
+++ Web/__tests__/page.test.jsx
@@ -39,17 +39,3 @@ test("cart toggles open/closed", async () => {
   await user.click(screen.getByRole("button", { name: /open cart/i }));
   expect(screen.getByTestId("cart")).toHaveTextContent("true");
 });
-
-test('renders recipes after loading', async () => {
-  render(<App />);
-
-  // check that loading shows up
-  expect(screen.getByText(/loading/i)).toBeInTheDocument();
-
-  // wait for the loading div to disappear
-  await waitFor(() => {
-    expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
-  });
-  // assert your recipes are there
-  expect(screen.getByText(/beavgredients/i)).toBeInTheDocument();
-});
